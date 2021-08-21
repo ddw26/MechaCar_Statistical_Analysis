492f7bcea8e90dc580b72f0984cab486acb60fc5
ddw26 <dwoods0898@protonmail.com>
2021-08-21 01:49:52 -0500

    updates

---
+++ MechaCarChallenge.R
@@ -0,0 +1,3 @@
+# Deliverable 1
+# 1. Download MechaCar_mpg.csv file; place it in active directory  R session.
+# 2. Create new RScript in R source pane, name it MechaCarChallenge.RScript; save it active directory.
